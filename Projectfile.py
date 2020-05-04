ce9ebc7a5217a0f87a89c1dbbdee000b978d5993
yahyaali <yahya09.ail@gmail.com>
2020-05-04 00:09:03 -0600

    Submit this Assignment

--- Projectfile.py
+++ Projectfile.py
@@ -1,13 +1,6 @@
 name = input("Name: ")
 print("hello " + name)
-print("welcome to my world")
-num("34", "343", "343", "34")
-print(num)
-dog = ("Barked at me")
-print(dog)
-"""
-les hope this reaches github. i am trying to figure how to 
-work my way backwords.
-it didnt work the second time
-les try the third
-"""
+if name == "yahya":
+    print("welcome to my world")
+else:
+    print("You are not welcome to this world")
